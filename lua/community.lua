ba297d18e4f47cba8d51e5668c70d76130005b1a
bl <latinovic@github.com>
2024-04-27 01:48:23 +0200

    My Conf

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,39 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      dim_inactive = { enabled = true, percentage = 0.25 },
+      highlight_overrides = {
+        mocha = function(c)
+          return {
+            Normal = { bg = c.mantle },
+            Comment = { fg = "#7687a0" },
+            ["@tag.attribute"] = { style = {} },
+          }
+        end,
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  {
+    "tpope/vim-fugitive",
+  },
+  {
+    "LunarVim/colorschemes",
+  },
+  {
+    "folke/tokyonight.nvim",
+  },
+  {
+    "iberianpig/tig-explorer.vim",
+  },
+  {
+    "czheo/mojo.vim",
+  },
 }
